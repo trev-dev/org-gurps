b05d4c292a411e7d0ff919ac981aeb2ffd9f5830
Trevor Richards <trev@trevdev.ca>
2022-03-20 12:51:20 -0700

    in the beginning there was nothing

---
+++ org-gurps.el
@@ -0,0 +1,34 @@
+;;; org-gurps --- A utility package for using org-mode to document GURPS campaigns.
+;;
+;; Author: Trevor Richards <trev@trevdev.ca>
+;; Version: 0.1.0
+;; Keywords: utility, org-mode
+;; URL: https://github.com/trev-dev/org-gurps
+;;
+;;; License:
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>
+;;
+;;; Commentary:
+;;
+;; This package is intended to enhance your campaign planning, documenting
+;; and journalling while using the Generic Universal RolePlaying System.  It is
+;; meant to be a companion in your toolkit with other tools like MapTool
+;; and GCS.
+;;
+;; This package is still a work in progress.
+;;
+;;; Code:
+
+(provide 'org-gurps)
+;;; org-gurps.el ends here
